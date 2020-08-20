2d03490ae118a0d8820424ca0e35ec0566e1eea5
jean96rq <jean96rq@gmail.com>
2020-08-20 16:48:24 -0500

    cambios en js

--- js/main.js
+++ js/main.js
@@ -61,16 +61,99 @@ window.onscroll = () =>{
     }
 }
 
-// ir Arriba con el boton 'irArriba'
+
+//scroll suave
+
+window.onload =()=>{
+    let links = document.querySelectorAll('.link')
+    let inicio = links[0]
+    inicio.addEventListener('click',()=>{
+        scrollSuave('#header',500,60)
+        document.querySelector('.btn-menu i').classList.toggle('fa-times');
+        menu.style.left='-100%'
+        menu.style.transition='0.5s'
+        activador=true;
+    })
+    let inicioDos = links[1]
+    inicioDos.addEventListener('click',()=>{
+        document.querySelector('.btn-menu i').classList.toggle('fa-times');
+        scrollSuave('#header',500,60)
+        menu.style.left='-100%'
+        menu.style.transition='0.5s'
+        activador=true;
+    })
+    let acercaDe = links[2]
+    acercaDe.addEventListener('click',()=>{
+        document.querySelector('.btn-menu i').classList.toggle('fa-times');
+        scrollSuave('#acercaDe',500,60)
+        menu.style.left='-100%'
+        menu.style.transition='0.5s'
+        activador=true;
+    })
+    let proyectos = links[3]
+    proyectos.addEventListener('click',()=>{
+        document.querySelector('.btn-menu i').classList.toggle('fa-times');
+        scrollSuave('#NuestrosProyectos',500,60)
+        menu.style.left='-100%'
+        menu.style.transition='0.5s'
+        activador=true;
+    })
+    let testimonios = links[4]
+    testimonios.addEventListener('click',()=>{
+        document.querySelector('.btn-menu i').classList.toggle('fa-times');
+        scrollSuave('#testimonios',500,60)
+        menu.style.left='-100%'
+        menu.style.transition='0.5s'
+        activador=true;
+    })
+    let contacto = links[5]
+    contacto.addEventListener('click',()=>{
+        document.querySelector('.btn-menu i').classList.toggle('fa-times');
+        scrollSuave('#footer',500,60)
+        menu.style.left='-100%'
+        menu.style.transition='0.5s'
+        activador=true;
+    })
+    let acercaDeDos = links[6]
+    acercaDeDos.addEventListener('click',()=>{
+        scrollSuave('#acercaDe',500,60)
+    })
+    
+}
+let scrollSuave =(objetivo,duracion,compensacion)=>{
+    let elemObj = document.querySelector(objetivo);
+    let elemPos = elemObj.getBoundingClientRect().top - compensacion;
+    let PosActual = window.pageYOffset;
+    let tiempoInical= null;
+
+    const animacion = tiempoAhora =>{
+        if(tiempoInical == null) tiempoInical = tiempoAhora
+        let tiempoPasado = tiempoAhora - tiempoInical
+        let auxAnimacion = easeInOutQuad(tiempoPasado,PosActual,elemPos,duracion);
+        window.scrollTo(0, auxAnimacion)
+        if(tiempoPasado<duracion) requestAnimationFrame(animacion)
+    }
+    requestAnimationFrame(animacion)
+
+}
+
+const easeInOutQuad = (t, b, c, d) => {
+    t /= d / 2
+    if (t < 1) return c / 2 * t * t + b
+    t--
+    return - c / 2 * (t * (t - 2) - 1) + b
+  }
+
+
 let btnVolverArriba = document.querySelector('.irArriba');
 btnVolverArriba.addEventListener('click',()=>{
     document.body.scrollTop=0;
     document.documentElement.scrollTop=0;
 })
-let btnIrAbajo = document.getElementById('abajo')
+// let btnIrAbajo = document.getElementById('abajo')
 
-btnIrAbajo.addEventListener('click',()=>{
-    document.body.scrollTop=600;
-    document.documentElement.scrollTop=600;
-})
+// btnIrAbajo.addEventListener('click',()=>{
+//     document.body.scrollTop=600;
+//     document.documentElement.scrollTop=600;
+// })
 
